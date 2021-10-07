4100bf330b7b5aab882eef096c5fe6942a0f266a
Alex020128 <alex20020128@gmail.com>
2021-10-07 01:27:05 -0400

    Added animation
    playerPickUp animation

--- Alex_Xu_HW5/Assets/Alex_Xu_HW5/Scene_1/Movement.cs
+++ Alex_Xu_HW5/Assets/Alex_Xu_HW5/Scene_1/Movement.cs
@@ -8,22 +8,36 @@ public class Movement : MonoBehaviour
     public float walkSpeed = 3.0f;
 
     public Rigidbody2D rb;
+    public SpriteRenderer sr;
     public Animator animator;
 
     Vector2 movement;
 
     void Walk()
     {
-        movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
+        if (!Input.GetKeyDown(KeyCode.Space))
+        {
+            movement.x = Input.GetAxisRaw("Horizontal");
+            movement.y = Input.GetAxisRaw("Vertical");
+        }
 
     }
 
-    // Update is called once per frame
-    void Update()
+    void pickUp()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            animator.SetTrigger("pickUp");
+        }
+    }
+
+// Update is called once per frame
+void Update()
     {
         Walk();
 
+        pickUp();
+        
         animator.SetFloat("Horizontal", movement.x);
         animator.SetFloat("Vertical", movement.y);
         animator.SetFloat("Speed", movement.sqrMagnitude);
